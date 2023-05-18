bc50d6e49f2d284f0ecc206710ff8ff450cf4fc9
kiwi334334 <99292273+kiwi334334@users.noreply.github.com>
2023-05-18 14:40:08 +0000

    modified:   index.html 	new file:   package.json	modified:   src/script.js 	modified:   src/style.css

--- src/script.js
+++ src/script.js
@@ -3,3 +3,4 @@ window.onmousemove = e => {
     document.getElementById('leftside').style.width = `${e.clientX / window.innerWidth * 100}%`;
 }
 document.ontouchmove = e => handleMove(e.touches[0]);
+
